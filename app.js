41a59d9920b08327c36bcf9abfd6dec04de679c4
Adii-tii <sableaditi8@gmail.com>
2026-05-08 22:39:08 +0530

    ok

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Jenkins + Docker + GitHub working!");
+  res.send("updated shi!");
 });
 
 app.listen(3000, () => {
